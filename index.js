693e727f231709c4bb7238b201a7472c82d7b93d
Artem Zhydkov <dtema1996@gmail.com>
2019-01-10 18:04:14 +0200

    third commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // Import a library to help create a component
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
-import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList';
-
-// Create a component
-const App = () => (
-  <View>
-    <Header headerText='Albums' />
-    <AlbumList />
-  </View>
-
-);
+import { AppRegistry } from 'react-native';
+import { App } from './App';
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const {
     title,
-     artist,
-      thumbnail_image
+    artist,
+    thumbnail_image,
+    image,
+    url
   } = album;
   const {
     headerContentStyle,
     thumbnailStyle,
     thumbnailContentStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
 
   return (
@@ -29,6 +33,15 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
@@ -51,6 +64,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
